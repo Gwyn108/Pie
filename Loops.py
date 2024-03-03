9562ffb2121a1faf44e79329e8b43b65c8263d19
Gwyn108 <gwyn424@gmail.com>
2024-03-03 13:27:31 +0000

    Loops.py  added

---
+++ Loops.py
@@ -0,0 +1,18 @@
+name ="Harriet"
+for char in name:
+   print(char)
+
+n=0
+for i in [0,1,2,3,4,5,6,7] : 
+   n=n+1
+   print(i) 
+   print(n) 
+# using range
+   # print numbers using range
+   print("using range")
+   for i in range(n):
+      print (i )  
+      #This type of loop can work for any sequence! For example, if we wish to print each name in a list, 
+      names=["wil","Paul","Kate"]
+      for name in names:
+         print(name)
